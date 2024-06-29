8140d2082030f0d5158005a9e85960be902329d9
Apollo Miller <apollo.miller@yahoo.com>
2024-06-29 10:23:36 -0400

    Completed js code for image generator project

--- js/script.js
+++ js/script.js
@@ -6,7 +6,30 @@ const img = document.querySelector(".img");
 const getImage = async function () {
 const res = await fetch("https://picsum.photos/v2/list?limit=100");
 const images = await res.json();
-console.log(images);
+selectRandomImage(images);
+
+//console.log(images);
 };
 
-getImage();
+
+
+const selectRandomImage = function (images) {
+const randomIndex = Math.floor(Math.random() * images.length);
+//console.log(randomIndex); test to see if random whole number between 0 and 99 is being pulled
+const randomImage = images[randomIndex];
+//console.log(randomImage);
+displayImage(randomImage);
+};
+
+const displayImage = function(randomImage) {
+const author = randomImage.author;
+const imageAddress = randomImage.download_url;
+authorSpan.innerText = author;
+ img.src = imageAddress;
+imgDiv.classList.remove("hide");
+
+
+};
+button.addEventListener("click", function () {
+    getImage();
+});
